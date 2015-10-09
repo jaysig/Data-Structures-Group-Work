4bcbe02bb631960d70bf43e134a046fc16f03a36
Jaysig <jasontsigmon@gmail.com>
2015-10-09 13:03:49 -0400

    Completed LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,29 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(!list.head){
+      list.head = Node(value);
+    }
+      list.tail = Node(value);
+      if(!list.head.next){
+        list.head.next = list.tail;
+      }
   };
 
   list.removeHead = function(){
+    if(list.head){
+      var result = list.head.value;
+      list.head = list.tail;
+      return result;
+    }
   };
 
   list.contains = function(target){
+    if(list.head.value === target || list.tail.value === target){
+      return true;
+    } else {
+      return false;
+    }
   };
 
   return list;
