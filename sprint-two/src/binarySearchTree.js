5d4dee15c9d992a86ac6707c441388b349c0a6fc
Jaysig <jasontsigmon@gmail.com>
2015-10-10 16:38:04 -0400

    Finished BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,42 +1,62 @@
 var BinarySearchTree = function(value){
-  this.value = null;
+  this.value = value;
   this.left = null;
   this.right = null;
 };
-var bst = new BinarySearchTree();
 
 BinarySearchTree.prototype.insert = function(value){
-  //No values - top value
   //If one - lower value left - higher value right
-  if(!this.value){
-    this.value =value;
-  }else{
-    if(this.values[0]===undefined){
-      this.values[0] = value;
-    }
-  }
-
+      if(this.left === null && value < this.value){
+        this.left = new BinarySearchTree(value);
+      } else if(this.right === null && value > this.value){
+        this.right = new BinarySearchTree(value);
+      } else if(value < this.value) {
+        // console.log(this.left);
+        this.left.insert(value);
+      }else {
+        this.right.insert(value);
+      }
 };
 BinarySearchTree.prototype.contains = function(value){
-  /*
-  Search through the tree to see if an item is contained
-  return _.reduce(this.values,function(wasFound,treeItem){
+  var tree = this;
+  var values = [];
+  var reducer = function(obj){
+    for(var key in obj){
+      if(typeof obj[key] === 'number'){
+        values.push(obj[key]);
+      } else if (typeof obj[key] === 'object' && obj[key] !== null){
+          reducer(obj[key]);
+      }
+    }
+  };
+  reducer(tree);
+  return _.reduce(values,function(wasFound,treeItem){
     if (wasFound){
     return true;
-  } else{
-  return treeItem === value;
-}
-//},false)
-*/
+  } else {
+    return treeItem === value;
+  }
+},false);
+
 };
-BinarySearchTree.prototype.depthFirstLog = function(callback){
-  /*
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+  var tree = this;
+  var values = [];
+  var reducer = function(obj) {
+    for (var key in obj) {
+      if (typeof obj[key] === 'number') {
+        values.push(obj[key]);
+      } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+        reducer(obj[key]);
+      }
+    }
+  };
+  reducer(tree);
   var newTree = [];
-  newTree = _.map(this.values,function(treeItem){
-    callback(item);
-})
-return newTree;
-  */
+  newTree = _.map(values, function(treeItem) {
+    callback(treeItem);
+  });
+  return newTree;
 };
 
 
