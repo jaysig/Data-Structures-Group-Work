b60d2ec3322296c1e0719a82b64814839987b1ec
Jaysig <jasontsigmon@gmail.com>
2015-10-10 14:11:38 -0400

    Finished HT starting BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,45 @@
 var BinarySearchTree = function(value){
+  this.value = null;
+  this.left = null;
+  this.right = null;
 };
+var bst = new BinarySearchTree();
+
+BinarySearchTree.prototype.insert = function(value){
+  //No values - top value
+  //If one - lower value left - higher value right
+  if(!this.value){
+    this.value =value;
+  }else{
+    if(this.values[0]===undefined){
+      this.values[0] = value;
+    }
+  }
+
+};
+BinarySearchTree.prototype.contains = function(value){
+  /*
+  Search through the tree to see if an item is contained
+  return _.reduce(this.values,function(wasFound,treeItem){
+    if (wasFound){
+    return true;
+  } else{
+  return treeItem === value;
+}
+//},false)
+*/
+};
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+  /*
+  var newTree = [];
+  newTree = _.map(this.values,function(treeItem){
+    callback(item);
+})
+return newTree;
+  */
+};
+
+
 
 
 /*
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,49 +5,75 @@ var HashTable = function(){
 
 };
 
-HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(k, v) {
+    var i = getIndexBelowMaxForKey(k, this._limit);
+    var tuple = [k, v];
+    if (this._storage[i] === undefined) {
+      this._storage[i] = [1];
+      //works
+    }
+    if (Array.isArray(this._storage[i])) {
+        for (var a = 0; a < this._storage[i].length; a++) {
+
+//           console.log(k);
+          if(this._storage[i][a] === 1){
+            this._storage[i][a] = []; //Zero Index create an array
+            this._storage[i][a].push(tuple); //first run
+          } else if (this._storage[i][a][0][0] === k) {
+            this._storage[i][a][0][1] = v;
+          } else {
+//             this._storage[i].push(tuple);
+            this._storage[i][a].push(tuple);
+          }
+        }
+      }
   //k is a string
   //at I pass a key and a value
-  if(typeof this._storage[i] !== 'object'){
-    this._storage[i] = {};
-  }
-  this._storage[i][k] = v;
+  // if(typeof this._storage[i] !== 'object'){
+  //   this._storage[i] = {};
+  // }
+  // this._storage[i][k] = v;
   //this._storage[i].push([k,v]);
 
 // roughly works
 //
 };
 
-HashTable.prototype.retrieve = function(k){
+HashTable.prototype.retrieve = function(k) {
   var i = getIndexBelowMaxForKey(k, this._limit);
+  for (var a = 0; a < this._storage[i].length; a++) {
+    if (Array.isArray(this._storage[i][a][0])) {
+      for (var b = 0; b < this._storage[i][a].length; b++) {
+        console.log(this._storage[i][a].length);
+        if (this._storage[i][a][b][0] === k) {
+          return this._storage[i][a][b][1];
+        }
+      }
+    } else if (this._storage[i][a][0] === k) {
+      return this._storage[i][a][1];
+    } else {
+      return null;
+    }
+  }
+};
   //k is a string
   // for(var w = 0;w<this._storage[i].length;w++){
   //   if(this._storage[i][w][0]===k){
   //     return this._storage[i][w][1];
   //   }
   // }
-  console.log(this._storage[i]);
-  return this._storage[i][k];
-  /*
-
-  if(Array.isArray(this._storage[i])){
-    for(var q = 0;q<this._storage[i].length;q++){
-      if(this._storage[i][q]===k){
-        return this._storage[i][q];
-      }
-    }
-  } else {
-    return this._storage[i];
-  }
-  return this._storage[i];
-  */
-
-};
+  // console.log(this._storage[i]);
+  // return this._storage[i][k];
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i][k] = null;
+  for(var a = 0; a<this._storage[i].length;a++){
+    if(this._storage[i][a][0][0]===k){
+      this._storage[i][a][0] = null;
+    }
+  }
+
+  // this._storage[i][k] = null;
   //Is this how I should delete it?
 };
 
@@ -57,27 +83,5 @@ HashTable.prototype.remove = function(k){
  * Complexity: What is the time complexity of the above functions?
  */
 
-
- //Insert
- //if k stays the same. i changes. Change v
- /*
- if(!this._storage[i]){
-   this._storage[i] = v;
- }
- */
-
- // else k is different. i is the same.
- /*
-  else {
-   if(typeof this._storage[i] === 'number'){
-     var z = this._storage[i];
-     this._storage[i] = [];
-     this._storage[i].push(z);
-   } else{
-     console.log(this._storage[i]);
-     this._storage[i].push(v);
-   }
- }
- */
 
  //store multiple values
